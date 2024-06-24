c9d556290016f8fab3d7fe7a4c84db431b974802
Tristan F <tristanfigueroareid@gmail.com>
2024-06-24 09:32:40 -0400

    refactor: componentize body end scripts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import Image from "next/image";
-import { useState, useEffect } from 'react';
-import Script from "next/script";
+import EndScripts from "./components/EndScripts";
 
 export default function Home() {
   return <>
@@ -1803,22 +1801,6 @@ export default function Home() {
   {/*==================================================*/}
   {/* Start Search Popup Area */}
   {/*==================================================*/}
-  <Script defer src="assets/js/vendor/modernizr-3.5.0.min.js" />
-  <Script defer src="assets/js/vendor/jquery-3.2.1.min.js" />
-  <Script defer src="assets/js/bootstrap.min.js" />
-  <Script defer src="assets/js/owl.carousel.min.js" />
-  <Script defer src="assets/js/jquery.counterup.min.js" />
-  <Script defer src="assets/js/waypoints.min.js" />
-  <Script defer src="assets/js/wow.js" />
-  <Script defer src="assets/js/imagesloaded.pkgd.min.js" />
-  <Script defer src="venobox/venobox.js" />
-  <Script defer src="assets/js/ajax-mail.js" />
-  <Script defer src="assets/js/animated-text.js" />
-  <Script defer src="venobox/venobox.min.js" />
-  <Script defer src="assets/js/isotope.pkgd.min.js" />
-  <Script defer src="assets/js/jquery.meanmenu.js" />
-  <Script defer src="assets/js/jquery.scrollUp.js" />
-  <Script defer src="assets/js/jquery.barfiller.js" />
-  <Script defer src="assets/js/theme.js" />
+  <EndScripts />
 </>
 }
