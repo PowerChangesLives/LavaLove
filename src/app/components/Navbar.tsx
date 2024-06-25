d27ec37e04f7a7adbe1c35fb17c016099aa8219b
Tristan F. <tristanfigueroareid@gmail.com>
2024-06-25 10:38:18 -0400

    refactor: componentize footer & navbar
    Co-Authored-By: HiruyWorku <153773845+HiruyWorku@users.noreply.github.com>

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,254 @@
+export default function Navbar() {
+    return <>
+        {/*==================================================*/}
+      {/* Start charina Main Menu Area */}
+      {/*==================================================*/}
+      <div
+        id='sticky-header'
+        className='charina_nav_manu style-three align-items-center'
+      >
+        <div className='container'>
+          <div className='row align-items-center'>
+            <div className='col-lg-2'>
+              <div className='logo'>
+                <a className='logo_img' href='index.html' title='charina'>
+                  <img src='assets/images/logo.png' alt='' />
+                </a>
+                <a className='main_sticky' href='index.html' title='charina'>
+                  <img src='assets/images/logo2.png' alt='astute' />
+                </a>
+              </div>
+            </div>
+            <div className='col-lg-8'>
+              <nav className='charina_menu'>
+                <ul className='nav_scroll'>
+                  <li>
+                    <a href='#'>
+                      Home{' '}
+                      <span>
+                        <i className='bi bi-chevron-down' />
+                      </span>
+                    </a>
+                    <ul className='sub-menu'>
+                      <li>
+                        <a href='index.html'>Home Page 01</a>
+                      </li>
+                      <li>
+                        <a href='index-2.html'>Home Page 02</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href='#'>
+                      Pages{' '}
+                      <span>
+                        <i className='bi bi-chevron-down' />
+                      </span>
+                    </a>
+                    <ul className='sub-menu'>
+                      <li>
+                        <a href='about.html'>About</a>
+                      </li>
+                      <li>
+                        <a href='volunteer.html'>Volunteer</a>
+                      </li>
+                      <li>
+                        <a href='volunteer-2.html'>Volunteer Two</a>
+                      </li>
+                      <li>
+                        <a href='gallery.html'>Gallery </a>
+                      </li>
+                      <li>
+                        <a href='gallery-details.html'>Gallery Details</a>
+                      </li>
+                      <li>
+                        <a href='contact-us.html'>Contact Us</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href='#'>
+                      Donations{' '}
+                      <span>
+                        <i className='bi bi-chevron-down' />
+                      </span>
+                    </a>
+                    <ul className='sub-menu'>
+                      <li>
+                        <a href='donate.html'>Donate</a>
+                      </li>
+                      <li>
+                        <a href='donations.html'>Donations</a>
+                      </li>
+                      <li>
+                        <a href='donations-details.html'>Donations Details</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href='#'>
+                      Events{' '}
+                      <span>
+                        <i className='bi bi-chevron-down' />
+                      </span>
+                    </a>
+                    <ul className='sub-menu'>
+                      <li>
+                        <a href='events.html'>Events</a>
+                      </li>
+                      <li>
+                        <a href='event-details.html'>Event Details</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href='#'>
+                      Blog{' '}
+                      <span>
+                        <i className='bi bi-chevron-down' />
+                      </span>
+                    </a>
+                    <ul className='sub-menu'>
+                      <li>
+                        <a href='blog-grid.html'>Blog Grid</a>
+                      </li>
+                      <li>
+                        <a href='blog.html'>Blog List</a>
+                      </li>
+                      <li>
+                        <a href='blog-details.html'>Blog Details</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href='contact-us.html'>Contact</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div className='col-lg-2 pl-0'>
+              <div className='header-button'>
+                <a href='donate.html'>
+                  {' '}
+                  Donate Now <i className='bi bi-suit-heart' />{' '}
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* charina Mobile Menu Area */}
+      <div className='mobile-menu-area d-sm-block d-md-block d-lg-none sticky'>
+        <div className='mobile-menu'>
+          <nav className='charina_menu'>
+            <ul className='nav_scroll'>
+              <li>
+                <a href='#'>
+                  Home{' '}
+                  <span>
+                    <i className='bi bi-chevron-down' />
+                  </span>
+                </a>
+                <ul className='sub-menu'>
+                  <li>
+                    <a href='index.html'>Home Page 01</a>
+                  </li>
+                  <li>
+                    <a href='index-2.html'>Home Page 02</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href='#'>
+                  Pages{' '}
+                  <span>
+                    <i className='bi bi-chevron-down' />
+                  </span>
+                </a>
+                <ul className='sub-menu'>
+                  <li>
+                    <a href='about.html'>About</a>
+                  </li>
+                  <li>
+                    <a href='volunteer.html'>Volunteer</a>
+                  </li>
+                  <li>
+                    <a href='volunteer-2.html'>Volunteer Two</a>
+                  </li>
+                  <li>
+                    <a href='gallery.html'>Gallery </a>
+                  </li>
+                  <li>
+                    <a href='gallery-details.html'>Gallery Details</a>
+                  </li>
+                  <li>
+                    <a href='contact-us.html'>Contact Us</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href='#'>
+                  Donations{' '}
+                  <span>
+                    <i className='bi bi-chevron-down' />
+                  </span>
+                </a>
+                <ul className='sub-menu'>
+                  <li>
+                    <a href='donate.html'>Donate</a>
+                  </li>
+                  <li>
+                    <a href='donations.html'>Donations</a>
+                  </li>
+                  <li>
+                    <a href='donations-details.html'>Donations Details</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href='#'>
+                  Events{' '}
+                  <span>
+                    <i className='bi bi-chevron-down' />
+                  </span>
+                </a>
+                <ul className='sub-menu'>
+                  <li>
+                    <a href='events.html'>Events</a>
+                  </li>
+                  <li>
+                    <a href='event-details.html'>Event Details</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href='#'>
+                  Blog{' '}
+                  <span>
+                    <i className='bi bi-chevron-down' />
+                  </span>
+                </a>
+                <ul className='sub-menu'>
+                  <li>
+                    <a href='blog-grid.html'>Blog Grid</a>
+                  </li>
+                  <li>
+                    <a href='blog.html'>Blog List</a>
+                  </li>
+                  <li>
+                    <a href='blog-details.html'>Blog Details</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href='contact-us.html'>Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      {/*==================================================*/}
+      {/* End charina Main Menu Area */}
+      {/*==================================================*/}
+    </>
+}
