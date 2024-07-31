cfbe8df992fe509a88cfd3ef9e0107a222a01bc3
Diya1201 <sanghvidiya@gmail.com>
2024-07-31 09:40:27 -0400

    subpage commit

--- src/app/(normal)/donate/hygine/page.tsx
+++ src/app/(normal)/donate/hygine/page.tsx
@@ -1,4 +1,7 @@
 import Breatcam from '@/app/components/Breatcam';
+//import './hygine.css';
+import './styles.css'; 
+//import './design.css';
 
 export default function Hygiene() {
   return (
@@ -6,6 +9,93 @@ export default function Hygiene() {
       <Breatcam breadcrumbs={['Donations', 'Hygiene']}>
         DIY Hygiene Kits
       </Breatcam>
+      {/*------------*/} 
+      <section className="u-clearfix u-section-1" id="carousel_e3e3">
+        <div className="u-clearfix u-sheet u-sheet-1">
+          <div className="u-container-style u-group u-image u-image-tiles u-image-1" data-image-width="100" data-image-height="60">
+            <div className="u-container-layout u-container-layout-1">
+              <div className="u-gradient u-shape u-shape-rectangle u-shape-1"></div>
+            </div>
+          </div>
+          <div className="u-hidden-md u-hidden-sm u-hidden-xs u-palette-2-base u-shape u-shape-rectangle u-shape-2"></div>
+          <img className="u-hidden-sm u-hidden-xs u-image u-image-2" src="/assets/images/HYGIENEKIT110420211.jfif" alt="Hygiene Kit" />
+          <div className="u-container-style u-expanded-width-xs u-group u-group-2">
+            <div className="u-container-layout u-container-layout-2">
+              {/*<h2 className="u-align-left u-custom-font u-text u-text-body-alt-color u-text-1">DIY Hygiene Kits</h2>*/}
+              <p className="u-align-justify u-text u-text-grey-15 u-text-2">
+                Hygiene supplies and kits are needed to ensure each guest is provided superior services. These can be purchased or DIY assembled as an individual, family, or group project and shipped directly to us at:<br />
+                <br /><b>403 Welsh Place, Morris Plains, NJ 07950</b>.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Add other sections as needed */}
+      <section className="skrollable skrollable-between u-align-center u-clearfix u-gradient u-hidden-md u-hidden-sm u-hidden-xs u-section-2" id="sec-6ba5"> 
+        <div className="u-clearfix u-sheet u-sheet-1">
+          <ul className="u-align-left u-text u-text-body-alt-color u-text-1">
+            <li>  17&quot; Backpack (Black Only) </li>
+            <li>2 Pairs of 3-Ply Disposable Protection Masks </li>
+            <li>50&quot; x 60&quot; Fleece Blanket </li>
+            <li>1 Pair of Men&apos;s Cotton Crew Socks (AssortedColors)</li>
+            <li>1 Pair of Adult Knitted Gloves (Assorted Colors) </li>
+            <li>1 Adult Knit Hat Beanie (Assorted Colors) </li>
+            <li>1 Oz Hand Sanitizer </li>
+            <li>1 Disposable Rain Ponchos (Assorted Colors) </li>
+            <li>1 Pair of Hand Warmers </li>
+            <li>Two 0.6 Oz Fluoride Toothpaste </li>
+            <li>Pediatric Toothbrush </li>
+            <li>1 Oz Shampoo</li>
+          </ul>
+          <ul className="u-align-left u-text u-text-body-alt-color u-text-2">
+            <li>1 Oz Body Wash</li>
+            <li>Two Deodorant Soaps </li>
+            <li>5&quot; Black Comb </li>
+            <li>Single Blade Razor </li>
+            <li>Two 0.18 Oz Shaving Creams </li>
+            <li>4.5&quot; Nail Filer </li>
+            <li>18.5&quot; Shower Cap </li>
+            <li>6 Thread Sewing Kits With Buttons </li>
+            <li>Personal fans with spray attachments</li>
+            <li>Extra batteries &nbsp;</li>
+            <li>Resealable Clear Bag</li>
+          </ul>
+        </div>
+      </section>
+      <section className="u-align-center u-clearfix u-gradient u-valign-middle u-section-3" id="carousel_3204">
+        <div className="u-align-left u-container-style u-expanded-width u-grey-25 u-group u-group-1">
+          <div className="u-container-layout u-container-layout-1"></div>
+        </div>
+        <div className="u-list u-list-1">
+          <div className="u-repeater u-repeater-1">
+            <div className="u-align-center u-container-style u-list-item u-opacity u-opacity-90 u-radius-20 u-repeater-item u-shape-round u-white u-list-item-1">
+              <div className="u-container-layout u-similar-container u-container-layout-2">
+                <h4 className="u-custom-font u-font-raleway u-text u-text-1">
+                  Hygiene Kits &nbsp; <br/>
+                </h4>
+                <a href="https://bagsinbulk.com/collections/hygiene-kits-supplies/products/premium-warm-hygiene-kit-includes-backpack-socks-blanket-hat-gloves-sanitizer-rain-poncho-hand-warmers-15-toiletries" className="u-border-none u-btn u-btn-round u-button-style u-gradient u-none u-radius-20 u-text-body-alt-color u-btn-1" target="_blank">PURCHASE HERE</a>
+              </div>
+            </div>
+            <div className="u-align-center u-container-style u-list-item u-opacity u-opacity-90 u-radius-20 u-repeater-item u-shape-round u-white u-list-item-2">
+              <div className="u-container-layout u-similar-container u-container-layout-3">
+                <h4 className="u-custom-font u-font-raleway u-text u-text-2">
+                  Crud Cloths &nbsp;<br/>
+                </h4>
+                <a href="https://www.crudcloth.com/products/crud-cloth-variety-pack" className="u-border-none u-btn u-btn-round u-button-style u-gradient u-none u-radius-20 u-text-body-alt-color u-btn-2" target="_blank">PURCHASE HERE</a>
+              </div>
+            </div>
+            <div className="u-align-center u-container-style u-list-item u-opacity u-opacity-90 u-radius-20 u-repeater-item u-shape-round u-white u-list-item-3">
+              <div className="u-container-layout u-similar-container u-container-layout-4">
+                <h4 className="u-custom-font u-font-raleway u-text u-text-3">
+                  Bulk Laundry Pods &nbsp;<br/>
+                </h4>
+                <a href="https://www.walmart.com/ip/all-Mighty-Pacs-Laundry-Detergent-Pacs-60-Count-Original-4-in-1-Tub/842409541" className="u-border-none u-btn u-btn-round u-button-style u-gradient u-none u-radius-20 u-text-body-alt-color u-btn-3" target="_blank">PURCHASE HERE</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
     </>
   );
 }
